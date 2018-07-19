9d909379147951f93f224f33add650c040de6ff8
Poly Goveia <poly.goveia@gmail.com>
2018-07-18 17:16:15 -0700

    refactor code

--- js/script.js
+++ js/script.js
@@ -1,47 +1,40 @@
 //Variables
-//select grid in html, and create a grid var
 const grid = document.getElementById("pixelCanvas");
-//grab the size picker
-const gridSize = document.getElementById("sizePicker");
-//grab color picker
 const cellColor = document.getElementById('colorPicker');
-// grab form to listen for submit
-const form = document.getElementById('sizePicker')
+const form = document.getElementById('sizePicker');
 
 // click submit to create grid
-gridSize.addEventListener("submit", makeGrid, false);
+form.addEventListener("submit", makeGrid);
 
-function makeGrid() {
-  // so grid doesnt colapse;
-  // event.preventDefault();
+
+function makeGrid(event) {
   // clear grid
-grid.innerHTML = '';
+  grid.innerHTML = '';
   //grid height
-const gridHeight = document.getElementById("inputHeight").value;
-//grid width
-const gridWidth = document.getElementById("inputWidth").value;
+  event.preventDefault();
+  const gridHeight = document.getElementById("inputHeight").value;
+  //grid width
+  const gridWidth = document.getElementById("inputWidth").value;
   // grid loop
   for (let r = 0; r < gridHeight; r++) {
     let row = document.createElement('tr');
-  for (let c = 0; c < gridWidth; c++) {
-    let col = document.createElement('td');
-    row.appendChild(col);
-   }
-   grid.appendChild(row);
- }
+    for (let c = 0; c < gridWidth; c++) {
+      let col = document.createElement('td');
+      row.appendChild(col);
+    }
+    grid.appendChild(row);
+  }
 
 }
+
+grid.addEventListener("click", paint);
+
 // paint function
-grid.addEventListener("click", function(event) {
+function paint (event) {
   // find out which element triggered event
-  console.log(event)
   let trigger = event.target;
   // use event to fill in cell
   if (trigger.tagName == "TD") {
     trigger.style.background = cellColor.value;
   }
-});
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-})
+};
